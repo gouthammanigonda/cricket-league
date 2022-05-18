92b06bfb515a7c62492aa33a619ab984c4e7196c
gouthammanigonda <gouthammanigonda737@gamil.com>
2022-05-18 16:36:05 +0000

    cricket league

---
+++ index.js
@@ -0,0 +1,11 @@
+
+const element = (
+//   Write your code here.
+    const element = (
+        <div className = "bg-container">
+            <h1>hi</h1>
+        </div>
+    )
+)
+
+ReactDOM.render(element, document.getElementById('root'))
